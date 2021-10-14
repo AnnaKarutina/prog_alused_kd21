d7b8d7e7a6709b987ffeb0f61a9d864c0ec55087
Anna Karutina <anna.karutina@khk.ee>
2021-10-14 09:01:56 +0300

    Teksti väljastamine konsooli ja kommenteerimine

---
+++ app.js
@@ -0,0 +1,15 @@
+// teksti väljastamine
+// kommentaaride kasutamine
+
+// üherealine kommentaar
+
+/*
+mitme
+realine
+kommentaar
+ */
+
+console.log("Tere Maailm")
+console.log('See on teine rida')
+
+console.log('See ' + 'on ' + ' liitlause')
